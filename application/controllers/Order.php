8fe5219f31a92fc0edb49b407301a2e5bc99dc7f
hahoanghai <hahoanghai.ueh@gmail.com>
2016-02-10 17:56:50 -0800

    handle checkout

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -87,13 +87,24 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
         //FIXME
+        $this->data['total'] = number_format($this->orders->total($order_num), 2);
 
+        $items = $this->orderitems->group($order_num);
+        foreach ($items as $item) {
+            $menuitem = $this->menu->get($item->item);
+            $item->code = $menuitem->name;
+        }
+        $this->data['items'] = $items;
+        
+        if(!$this->orders->validate($order_num))
+            $this->data['okornot'] = "disabled";   
         $this->render();
     }
 
     // proceed with checkout
     function proceed($order_num) {
         //FIXME
+
         redirect('/');
     }
 
--- application/models/Orders.php
+++ application/models/Orders.php
@@ -17,7 +17,7 @@ class Orders extends MY_Model {
         $CI = & get_instance();
         if ($CI->orderitems->exists($num, $code))
         {
-            $record = $CI->exists($num, $code);
+            $record = $CI->orderitems->get($num, $code);
             $record->quantity++;
             $CI->orderitems->update($record);
         }else
@@ -56,7 +56,15 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+        $CI = & get_instance();
+        $items = $CI->orderitems->group($num);
+        $gotem = array();
+        if(count($items) > 0)
+            foreach($items as $item){
+                $menu = $CI->menu->get($item->item);
+                $gotem[$menu->category] = 1;
+            }
+        return isset($gotem['m']) && isset($gotem['d']) && isset($gotem['s']);
     }
 
 }
